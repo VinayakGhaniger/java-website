7fefc66853c8883b1bd81346c32a911d61277647
vinayak <vinayakganiger217@gmail.com>
2026-01-28 09:44:51 +0530

    i commited

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,6 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Java Website Build Successful!");
-        System.out.println("Java Website Builded Successful!");
     }
     }
 
