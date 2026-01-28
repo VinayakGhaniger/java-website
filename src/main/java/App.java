6f16b3096598a6afb39bee4ca5fb372b213428ba
vinayak <vinayakganiger217@gmail.com>
2026-01-28 09:10:30 +0530

    update in app.jajava

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,5 +2,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Java Website Build Successful!");
+        System.out.println("Java Website Builded Successful!");
     }
-}
+    }
+
